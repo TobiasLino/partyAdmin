7c44326837671bd79c7028fde330c9a8146df8f0
TobiasLino <tobias.lino@sonda.com>
2022-09-25 20:42:02 -0300

    initial setup

---
+++ src/test/java/br/toba/party/PartyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.toba.party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
